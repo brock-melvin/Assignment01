c9a65de9fa892dd1834d3f60ba8ee52638a26f78
Brock Melvin <bmelvin9@huskers.unl.edu>
2020-08-29 23:26:26 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author:  Brock Melvin
+ * Date:    2020/08/29
+ *
+ * A simple hello world program in C that prints the author's name
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+
+ int main(int argc, char **argv) {
+
+     printf("Brock Melvin!\n");
+
+     return 0;
+
+ }
